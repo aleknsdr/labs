2dfa6eccda7167668c46141ca1e910cb8da24088
aleknsdr <arutyunyan1452@gmail.com>
2023-12-17 21:20:54 +0300

    add AddressServlet

---
+++ src/main/java/org/example/labs/AddressServlet.java
@@ -0,0 +1,33 @@
+package org.example.labs;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "addressServlet", value = "/address-servlet")
+public class AddressServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public AddressServlet(){
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        try {
+            writer.println("<h2>Привет AddressServlet</h2>");
+        }finally {
+            writer.close();
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
